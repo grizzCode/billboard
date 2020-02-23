2866bffeef33e33652ef9e717c447515c1545f18
Charles Buxton <chazbux@gmail.com>
2020-02-22 22:40:22 -0700

    progress update

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,13 +1,54 @@
 class SongsController < ApplicationController
-  def index
-  end
+  before_action :set_artist
+  before_action :set_song, only: [:show, :edit, :update, :destroy]
+   def index
+     @songs = @artist.songs
+   end
+   
+   def show
+   end
+   
+   def new
+     @song = Song.new
+   end
+   
+   def create
+     @song = @artist.songs.new(songs_params)
+     if @song.save
+       redirect_to [@artist, @song]
+     else
+       render :new
+     end
+   end
+    
+   def edit
+   end
+   
+   def update
+     if @song.update(songs_params)
+       redirect_to [@artist, @song]
+     else
+       render :edit
+     end
+   end
+   
+   def destroy
+     @song.destroy
+     redirect_to artist_songs_path(@artist)
+   end
+   
+   private
 
-  def new
+    def set_song
+     @song = Song.find(params[:id])
+   end
+   
+   def set_artist
+      @artist= Artist.find(params[:artist_id])
+   end
+   
+   def songs_params
+      params.require(:song).permit(:name, :genre)
+   end
   end
-
-  def edit
-  end
-
-  def show
-  end
-end
+  
