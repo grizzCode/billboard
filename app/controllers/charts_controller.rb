48b766afb6c1c61be23f24556b88fb4e4c63ce61
Charles Buxton <chazbux@gmail.com>
2020-02-23 18:30:16 -0700

    stable progress update

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -17,13 +17,13 @@ class ChartsController < ApplicationController
   def add_song
     @chart = Chart.find(params[:id])
     @chart.songs << Song.find(params[:song_id])
-    # redirect_to theater_path(@theater)
+    redirect_to chart_path(@chart)
   end
 
   def remove_song
     @chart = Chart.find(params[:id])
     Song.find(params[:song_id]).update(chart_id: nil)
-    # redirect_to theater_path(@theater)
+    redirect_to chart_path(@chart)
   end
 
 end
